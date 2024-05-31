0590a1437b2e5592a121ded8486efa5e3939ca22
f-Rodeh <rodri.leche@gmail.com>
2024-05-30 21:02:23 -0500

    Build prototype

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const generatorBt = document.getElementById('generator');
+const resultHolder = document.getElementById('result');
+generatorBt.onclick = generateExcuse
+
+const activities = ["un desfile", "una manifestación"];
+const participants = ["bomberos", "enanos", "payasos", "evangélicos", "reggaetoneros"];
+
+function generateExcuse(){
+    const i = random(activities.length);
+    const j = random(participants.length);
+    let result = `${activities[i]} de ${participants[j]}` //add emoji puntuation? 💀
+    resultHolder.textContent = result;
+}
+
+function random(maxExclusive){
+    return Math.floor(Math.random() * maxExclusive);
+}
