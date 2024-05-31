f187e8238b2e3bd3d3896e074d67d50c960972a5
f-Rodeh <rodri.leche@gmail.com>
2024-05-30 21:24:36 -0500

    Add content

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const generatorBt = document.getElementById('generator');
 const resultHolder = document.getElementById('result');
 generatorBt.onclick = generateExcuse
 
-const activities = ["un desfile", "una manifestación", "un concurso", "una caravana", "una protesta", "una marcha", "un concurso de baile", "una rifa"];
+const activities = ["un desfile", "una manifestación", "un concurso", "una caravana", "una protesta", "una marcha", "un concurso de baile", "una rifa", "un camioncito"];
 const participants = ["bomberos", "enanos", "payasos", "evangélicos", "reggaetoneros", "juegos de azar y mujerzuelas", "narcos", "marihuanos", "botargas", "Dr. Simis", "la USAC", "viejitas", "viejitas sabrosas", "edecanes"];
 
 function generateExcuse(){
